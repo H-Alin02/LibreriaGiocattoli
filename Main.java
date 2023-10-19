56275474296165ade0462b94f3c6119adcd1081a
Alin Marian Habasescu <habasescualinmarian@gmail.com>
2023-10-19 12:11:13 +0200

    Test dei metodi di giocattoliStatici

--- Main.java
+++ Main.java
@@ -1,17 +1,18 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        Peluche Peluche = new Peluche(19.50,"Disney","Peluche di cenerentola",30);
+        System.out.println("\n"+Peluche.gioca());
+        Peluche.descrizioneStatic();
 
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+        Costruzioni costruzioni = new Costruzioni(10,"Bho","costruzioni di tutti i colori",20);
+        System.out.println("\n"+costruzioni.gioca());
+        costruzioni.descrizioneStatic();
+
+        GiochiDiTavolo giochiDiTavolo = new GiochiDiTavolo(20,"Monopoly","Gioco classico monopoly","10-99",5);
+        System.out.println("\n" + giochiDiTavolo.gioca());
+        giochiDiTavolo.descrizioneStatic();
+
+
     }
 }
